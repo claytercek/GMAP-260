5cee5b83682432fb82801a6f4b611781335d86a3
claytercek <clay.tercek@gmail.com>
2017-12-04 22:19:48 -0500

    Added Waypoint

--- Assets/Scripts/CollisionDetection.cs
+++ Assets/Scripts/CollisionDetection.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CollisionDetection : MonoBehaviour
-{
+public class CollisionDetection : MonoBehaviour{
+	public Vector3 RespawnPosition;
 	public GameObject HurtOverlay;
 
+	void Awake() {
+		RespawnPosition = new Vector3 (-4.289f, -16.148f, 0);
+	}
 	void OnTriggerEnter2D (Collider2D other)
 	{
-		other.transform.position = new Vector3(-4.289f, -16.148f, 0);
+		other.transform.position = RespawnPosition;
 		HurtOverlay.GetComponent<Image>().CrossFadeAlpha(220, .01f, false);
 		HurtOverlay.GetComponent<Image>().CrossFadeAlpha(1, .5f, false);
 	}
---
+++ Assets/Scripts/waypoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waypoint : MonoBehaviour{
+	public Vector3 waypointPosition;
+	public Sprite GreenSwitch;
+	// Use this for initialization
+	void Start () {
+		waypointPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter2D (Collider2D other) {
+		gameObject.GetComponent<SpriteRenderer> ().sprite = GreenSwitch;
+		foreach(GameObject spikes in GameObject.FindGameObjectsWithTag("obstacle"))
+		{
+			spikes.GetComponent<CollisionDetection> ().RespawnPosition = waypointPosition;
+		}
+	}
+}
