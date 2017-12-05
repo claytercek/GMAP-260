d09897084e93c6c96705a5b9474b130784d237e9
claytercek <clay.tercek@gmail.com>
2017-12-05 14:22:10 -0500

    Fixed Build issues

---
+++ Assets/Scripts/stopClock.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stopClock : MonoBehaviour {
+	public GameObject clock;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter2D (Collider2D other) {
+		clock.GetComponent<Timer>().enabled = false;
+			
+	}
+}
